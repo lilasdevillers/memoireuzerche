7f77156013a34d68467e9aced73524672d9428ab
laura0007 <laura.hespert@gmail.com>
2021-04-05 12:27:46 +0200

    carte !

--- uzerche.R
+++ uzerche.R
@@ -534,8 +534,31 @@ canton <- correze@data$CODE_CANT==28|correze@data$CODE_CANT==29|correze@data$COD
 plot(correze[canton,],main="Population in Correze", sub="1874-1906 cohorts")
 points(coordinates(correze[correze@data$NOM_COMM %in% c("CONDAT-SUR-GANAVEIX","ESPARTIGNAC","EYBURIE","MASSERET","MEILHARDS","SAINT-YBARD","SALON-LA-TOUR" ,"UZERCHE","VIGEOIS", "TREIGNAC"),]), pch=20, 
        col=c("red","blue","green","yellow","brown","pink","orange","purple","cyan","black"), cex=1)
+#carte 3 : avec nos données
+pop$nom <- 0
+pop[pop$NOM_COMM=="UZERCHE",2] <-nrow(base[base$place=="uzerche"&base$annee=="1906",])
+pop[pop$NOM_COMM=="CONDAT-SUR-GANAVEIX",2] <-nrow(base[base$place=="condat-sur-ganaveix"&base$annee=="1906",])
+pop[pop$NOM_COMM=="ESPARTIGNAC",2] <-nrow(base[base$place=="espartignac"&base$annee=="1906",])
+pop[pop$NOM_COMM=="EYBURIE",2] <-nrow(base[base$place=="eyburie"&base$annee=="1906",])
+pop[pop$NOM_COMM=="MASSERET",2] <-nrow(base[base$place=="masseret"&base$annee=="1906",])
+pop[pop$NOM_COMM=="MEILHARDS",2] <-nrow(base[base$place=="meilhards"&base$annee=="1906",])
+pop[pop$NOM_COMM=="SAINT-YBARD",2] <-nrow(base[base$place=="saint-ybard"&base$annee=="1906",])
+pop[pop$NOM_COMM=="SALON-LA-TOUR",2] <-nrow(base[base$place=="salon-la-tour"&base$annee=="1906",])
+pop[pop$NOM_COMM=="VIGEOIS",2] <-nrow(base[base$place=="vigeois"&base$annee=="1906",])
+pop[pop$NOM_COMM=="TREIGNAC",2] <-nrow(base[base$place=="treignac"&base$annee=="1906",])
+correze@data$pop <- pop$nom
 
-#carte 3 : comme la 2 mais avec la méthode de gautier et les quantiles du nb pop
+library(classInt)
+nclasse <- 3
+distr <- classIntervals(correze@data$pop, nclasse, style="quantile")$brks
+library(RColorBrewer)
+colfunc <- colorRampPalette(c("lightpink3", "blue","lightblue4"))
+colours <- colfunc(nclasse)
+
+colMap <- colours[findInterval(correze$pop, distr, all.inside=TRUE)]
+par(mar=c(6,2.5,4,2))
+
+#carte 3 bis : comme la 2 mais avec la méthode de gautier et les quantiles du nb pop
 load("pop.RData")
 
 library(rgdal)
@@ -564,6 +587,17 @@ plot(mymap[canton,], col=colMap, main="Population in Correze")
 points(coordinates(mymap[mymap@data$NOM_COMM %in% c("CONDAT-SUR-GANAVEIX","ESPARTIGNAC","EYBURIE","MASSERET","MEILHARDS","SAINT-YBARD","SALON-LA-TOUR" ,"UZERCHE","VIGEOIS", "TREIGNAC"),]), pch=20, 
        col=c("red","blue","green","yellow","brown","pink","orange","purple","cyan","black"), cex=1)
 
+#carte 4 : la notre sans pop + vezère
+canton <- correze@data$CODE_CANT==28|correze@data$CODE_CANT==29|correze@data$CODE_CANT==24|correze@data$CODE_CANT==22
+plot(correze[canton,],main="Map of Correze")
+points(coordinates(correze[correze@data$NOM_COMM %in% c("CONDAT-SUR-GANAVEIX","ESPARTIGNAC","EYBURIE","MASSERET","MEILHARDS","SAINT-YBARD","SALON-LA-TOUR"),]), pch=20, 
+       col=c("black","black","black","black","black","black","black"), cex=1)
+points(coordinates(correze[correze@data$NOM_COMM %in% c("TREIGNAC" ,"UZERCHE","VIGEOIS"),]), pch=20, 
+       col=c("red","red","red"), cex=1)
+x <- locator(n=20)
+lines(x,col="lightblue1",lwd=2)
+# a tracer à la main
+
 #marche pas : tentative rajouter legende et nom des villes
 pointLabel(coordinates(correze[correze@data$NOM_COMM %in% c("CONDAT-SUR-GANAVEIX","ESPARTIGNAC","EYBURIE","MASSERET","MEILHARDS","SAINT-YBARD","SALON-LA-TOUR" ,"UZERCHE","VIGEOIS", "TREIGNAC"),]), 
            labels = correze@data$NOM_COMM[correze@data$NOM_COMM %in% c("CONDAT-SUR-GANAVEIX","ESPARTIGNAC","EYBURIE","MASSERET","MEILHARDS","SAINT-YBARD","SALON-LA-TOUR" ,"UZERCHE","VIGEOIS", "TREIGNAC")], 
@@ -571,3 +605,35 @@ pointLabel(coordinates(correze[correze@data$NOM_COMM %in% c("CONDAT-SUR-GANAVEIX
 legend("left",c("CONDAT-SUR-GANAVEIX","ESPARTIGNAC","EYBURIE","MASSERET","MEILHARDS","SAINT-YBARD","SALON-LA-TOUR" ,"UZERCHE","VIGEOIS", "TREIGNAC"),
        c("red","blue","green","yellow","brown","pink","orange","purple","cyan","black"), pch="1",cex=0.2)
 
+#truc pour tracer
+library(raster)
+adm_fr <- getData('GADM', country='FRA', level=2)
+plot(mymap[canton,])
+x <- locator(n=2) 
+# Là, je clique deux fois sur la carte pour faire un segment
+lines(x,col="red",lwd=3)
+# Là, je clique 5 fois sur la carte pour faire la ligne bleue
+x <- locator(n=10)
+lines(x,col="lightblue1",lwd=2)
+
+#carte au propre
+library("rgdal")
+correze <- readOGR(dsn="19-correze", layer="19-")
+correze <- spTransform(correze, CRS("+proj=longlat"))
+correze@data$id <- rownames(correze@data)
+#determination de la zone qui nous interesse
+plot(correze, col = "lightgrey")
+plot(correze[canton, ], col = "turquoise", add = TRUE)
+points(coordinates(correze[correze@data$NOM_COMM %in% c("CONDAT-SUR-GANAVEIX","ESPARTIGNAC","EYBURIE","MASSERET","MEILHARDS","SAINT-YBARD","SALON-LA-TOUR" ,"UZERCHE","VIGEOIS", "TREIGNAC"),]), 
+       pch=20, col="red", cex=1)
+#zoom sur cette zone + tracage vezere
+canton <- correze@data$CODE_CANT==28|correze@data$CODE_CANT==29|correze@data$CODE_CANT==24|correze@data$CODE_CANT==22
+plot(correze[canton,],main="Map of Correze")
+points(coordinates(correze[correze@data$NOM_COMM %in% c("CONDAT-SUR-GANAVEIX","ESPARTIGNAC","EYBURIE","MASSERET","MEILHARDS","SAINT-YBARD","SALON-LA-TOUR"),]), pch=20, 
+       col=c("black","black","black","black","black","black","black"), cex=1)
+points(coordinates(correze[correze@data$NOM_COMM %in% c("TREIGNAC" ,"UZERCHE","VIGEOIS"),]), pch=20, 
+       col=c("red","red","red"), cex=1)
+library(raster)
+x <- locator(n=20)
+lines(x,col="lightblue1",lwd=2)
+# a tracer à la main
